76a50f51f50943af0da2d7569c41c691c653e060
Jan Lewandowski <iasiu@icloud.com>
2020-03-29 23:28:57 +0200

    Initial Commit

---
+++ Pokedex/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pokedex
+//
+//  Created by Jan Lewandowski on 29/03/2020.
+//  Copyright © 2020 janlewandowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
