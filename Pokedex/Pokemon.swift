fa029481cab64a94aa1dabb900bc63d7c1ff9b5d
Jan Lewandowski <iasiu@icloud.com>
2020-03-30 23:48:19 +0200

    first commit

---
+++ Pokedex/Pokemon.swift
@@ -0,0 +1,33 @@
+//
+//  Pokemon.swift
+//  Pokedex
+//
+//  Created by Jan Lewandowski on 29/03/2020.
+//  Copyright © 2020 janlewandowski. All rights reserved.
+//
+
+import Foundation
+
+struct PokemonList: Codable {
+    let results: [Pokemon]
+}
+
+struct Pokemon: Codable {
+    let name: String
+    let url: String
+}
+
+struct PokemonData: Codable {
+    let id: Int
+    let types: [PokemonTypeEntry]
+}
+
+struct PokemonType: Codable {
+    let name: String
+    let url: String
+}
+
+struct PokemonTypeEntry: Codable {
+    let slot: Int
+    let type: PokemonType
+}
